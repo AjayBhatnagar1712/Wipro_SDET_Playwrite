809ce4bbdb24c7c6cd6a3390db969c77223b082b
Ajay Bhatnagar <ajaybhatnagar1712@gmail.com>
2026-04-30 17:15:03 +0530

    Updated multiple files and added new features

--- index.js
+++ index.js
@@ -3,24 +3,22 @@ let b = 10;
 
 let str = "Hello, World!";
 
-// String operations
 console.log("String:", str);
 console.log("Length of string:", str.length);
 console.log("Uppercase:", str.toUpperCase());
 console.log("Lowercase:", str.toLowerCase());
-console.log("Substring (0-5):", str.substring(0, 5)); // 0 – first character, 5 – up to but not including the 5th character
+console.log("Substring (0-5):", str.substring(0, 5)); 
 
-// Mathematical operations
 console.log("Addition:", a + b);
 console.log("Subtraction:", a - b);
 console.log("Multiplication:", a * b);
 console.log("Division:", a / b);
-console.log("Remainder:", a % b); // Remainder (Modulo)
+console.log("Remainder:", a % b); 
 
-// Comparison operations
-console.log("a > b:", a > b);
-console.log("a < b:", a < b);
-console.log("a == b:", a == b);
-console.log("a != b:", a != b);
-console.log("a >= b:", a >= b);
-console.log("a <= b:", a <= b);
+
+console.log("a > b:", a > b); 
+console.log("a < b:", a < b); 
+console.log("a == b:", a == b); 
+console.log("a != b:", a != b); 
+console.log("a >= b:", a >= b); 
+console.log("a <= b:", a <= b); 
---
+++ swap.js
@@ -0,0 +1,7 @@
+let a = 4;
+let b = 7;
+console.log("Before swap:", "a =", a, "b =", b); 
+let temp = a; 
+a = b;        
+b = temp;     
+console.log("After swap:", "a =", a, "b =", b); 
