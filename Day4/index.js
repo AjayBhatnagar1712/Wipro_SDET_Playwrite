0abcc87ad09909b0d4b90211aeb0bd5f002bc671
Ajay Bhatnagar <ajaybhatnagar1712@gmail.com>
2026-05-06 09:29:07 +0530

    Updated Day2 / latest changes

--- Day4/index.js
+++ Day4/index.js
@@ -1,41 +1,34 @@
-// function getData(){
-//     return new Promise((resolve,reject) =>{
-//         setTimeout(()=>{
-//             resolve("Data received");
-//         },2000);
-//     });
-// }
-
-// getData().then((data) =>{
-//     console.log(data);
-
-// }).catch((error)=>{
-//     console.log("Error: ", error);
-// });
-
-
-
-function getData(){
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            reject("Data received");
-        },2000);
+function getData() {
+    return new Promise((resolve, reject) => {
+        if(2 > 3) {
+ 
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
+        } else {
+            reject('not working')
+        }
     });
 }
-
-getData().then((data) =>{
-    console.log(data);
-
-}).catch((error)=>{
-    console.log("Error: ", error);
-});
-
-
-setTimeout(()=>{
-    console.log("Hello");
-},2000);
-
-
-setInterval(()=>{
-    console.log("Data checking again and again");
-},5000);
+ 
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
+ 
+// getData().then((data) => {
+//     console.log(data);
+// }).catch((error) => {
+//     console.error("Error:", error);
+// });
+ 
+// setTimeout(() => {
+//     console.log("Data received");
+// }, 5000);
+// console.log("test");
+ 
+// setInterval(() => {
+//     console.log("Checking for new data...");
+// }, 3000);
