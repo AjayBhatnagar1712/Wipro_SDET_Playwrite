61b947bccdbe344f5ffe1c98d424013e828943dd
Ajay Bhatnagar <ajaybhatnagar1712@gmail.com>
2026-05-11 09:36:36 +0530

    Updated Day9 / latest changes

---
+++ Learning Java Script/Day1/first.js
@@ -0,0 +1,8 @@
+let name =  "Ajay";
+let age =  24;
+let x = null;
+let y = undefined;
+let f = false;
+let t =true;
+let price = 99.894
+console.log(name);
---
+++ Learning Java Script/Day1/updating a variable.js
@@ -0,0 +1,18 @@
+let agee= 1;
+agee= 4;
+agee= 24;
+console.log(agee);
+let x = BigInt("234");
+
+const student = {
+    fullname : "Ajay", 
+    cgpa :7.1,
+    caste :"Kayastha",
+    income:0.0,
+    ispass : true,
+    age:23,
+}
+console.log(student);
+console.log(student["fullname"]);
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
